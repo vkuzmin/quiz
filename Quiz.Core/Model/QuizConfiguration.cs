a09b654dc67e83d2a5d857eaeed6c6ff4e290d32
Vadzim Kuzmin <vadimkuz@tut.by>
2015-12-25 13:53:28 +0300

    Initial project version

---
+++ Quiz.Core/Model/QuizConfiguration.cs
@@ -0,0 +1,17 @@
+﻿namespace Quiz.Core.Model
+{
+    public class QuizConfiguration
+    {
+        public int NumberOfRounds { get; set; }
+
+        public int NumberOfTopicsInRound { get; set; }
+
+        public int NumberOfQuestionsInTopic { get; set; }
+
+        public double MinCost { get; set; }
+
+        public double RoundCostFactor { get; set; }
+
+        public int ComplexityLevel { get; set; }
+    }
+}
---
+++ Quiz.Core/Model/QuizEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quiz.Core.Model
+{
+    public class QuizEntity
+    {
+        public Guid Id { get; set; }
+    }
+}
